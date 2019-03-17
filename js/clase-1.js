c75846d20c02b24e5186392b35d4c46d80999574
Anibal Ledesma <aledesmadev@gmail.com>
2019-03-16 19:49:42 -0500

    Clase 16

--- js/clase-1.js
+++ js/clase-1.js
@@ -1,71 +1,55 @@
 /* global*/
-const MAYORIA_DE_EDAD = 21;
+const KG = 0.2;
+const DIAS_DEL_ANIO = 365;
+
 var anibal = {
     nombre: 'Anibal',
     apellido: 'Ledesma',
     edad: 33,
-    ocupacion: 'Front End'
+    ocupacion: 'Front End',
+    peso: 84
 }
 var laura = {
     nombre: 'Laura',
     apellido: 'Ledesma',
     edad: 20,
-    ocupacion: 'Tecnóloga en quimica'
+    ocupacion: 'Tecnóloga en quimica',
+    peso: 50
 }
 
 var angela = {
     nombre: 'Angela',
     apellido: 'Ledesma',
     edad: 32,
-    ocupacion: 'Psicologa'
+    ocupacion: 'Psicologa',
+    peso: 70
 }
-
-// function imprimirDatosPersona({nombre}) {
-//     console.log(nombre);
-// }
-
-// function imprimirDatosPersona(persona) {
-//     console.log(persona.nombre);
-// }
-
-// function mayoriaDeEdad({edad}) {
-//     return edad >= MAYORIA_DE_EDAD;
-// }
-
-// var mayoriaDeEdad = function ({edad}) {
-//     return edad >= MAYORIA_DE_EDAD;
-// }
-
-// const mayoriaDeEdad = ({edad}) => {
-//     return edad >= MAYORIA_DE_EDAD;
-// }
-
-// const mayoriaDeEdad = persona => persona.edad >= MAYORIA_DE_EDAD;
-
-const mayoriaDeEdad = ({ edad }) => edad >= MAYORIA_DE_EDAD;
-
-
-function imprimirDatosPersona(persona) {
-    if (mayoriaDeEdad(persona)) {
+const aumentarPeso = persona => persona.peso += KG;
+const disminuyePeso = persona => persona.peso -= KG;
+const comeMucho = () => Math.random() < 0.3;
+const realizaDeporte = () => Math.random() < 0.4;
+var dias = 0;
+function validaMeta(persona) {
+    const META = persona.peso - 3;
+    while (persona.peso > META) {
+        // eslint-disable-next-line no-debugger
+        // debugger
+        if (comeMucho()) {
+            aumentarPeso(persona)
+        }
+        if(realizaDeporte()) {
+            disminuyePeso(persona)
+        }
+        dias += 1;
+    }
+    if (dias > DIAS_DEL_ANIO) {
         // eslint-disable-next-line no-console
-        console.log(`${persona.nombre} ${persona.apellido} es ${persona.ocupacion}, tiene ${persona.edad} años y es mayor de edad`);
+        console.log(`${persona.nombre} ${persona.apellido} logro su meta despues de ${dias} días ahora su peso es ${persona.peso.toFixed(1)}, aunque sobrepaso el año :(`)
     } else {
         // eslint-disable-next-line no-console
-        console.log(`${persona.nombre} ${persona.apellido} es ${persona.ocupacion}, tiene ${persona.edad} años y es menor de edad`);
+        console.log(`${persona.nombre} ${persona.apellido} logro su meta despues de ${dias} días ahora su peso es ${persona.peso.toFixed(1)} y no superó el año :)`)
     }
 }
-
-function permitirAcceso(persona) {
-    if (!mayoriaDeEdad(persona)) {
-        // eslint-disable-next-line no-console
-        console.log(`${persona.nombre} ¡su acceso ha sido denegado!`);
-    } else {
-        // eslint-disable-next-line no-console
-        console.log(`${persona.nombre} tiene ¡Acceso!`);
-    }
-}
-imprimirDatosPersona(anibal);
-imprimirDatosPersona(laura);
-imprimirDatosPersona(angela);
-permitirAcceso(laura);
-permitirAcceso(angela);
+validaMeta(anibal);
+validaMeta(angela);
+validaMeta(laura);
