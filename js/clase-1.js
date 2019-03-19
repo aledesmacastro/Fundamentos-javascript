5f154643e203e83d638047624a522bde05ca6b6d
Anibal Ledesma <aledesmadev@gmail.com>
2019-03-19 09:50:04 -0500

    Haciendo multiples callbacks

--- js/clase-1.js
+++ js/clase-1.js
@@ -1,11 +1,18 @@
-/* CALLBACKS*/
+/* HACIENDO MULTIPLES REQUEST*/
 const API_URL = 'https://swapi.co/api/';
 const PEOPLE_URL = 'people/:id';
-const lukeUrl = `${API_URL}${PEOPLE_URL.replace(':id', 4)}`;
 const opts = { crossDomain: true };
+// const lukeUrl = `${API_URL}${PEOPLE_URL.replace(':id', 4)}`;
 
 // eslint-disable-next-line no-console
 const onResponse = personaje => console.log(`Hola me llamo ${personaje.name}`);
-
-// eslint-disable-next-line no-undef
-$.get(lukeUrl, opts, onResponse);
+function obtenerPersonaje(id) {
+    const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`;
+    // eslint-disable-next-line no-undef
+    $.get(url, opts, onResponse);
+}
+obtenerPersonaje(1);
+obtenerPersonaje(2);
+obtenerPersonaje(3);
+obtenerPersonaje(4);
+obtenerPersonaje(5);
