3bbacdaa7da0edcd1e5b1e056fd66670e787d65e
Anibal Ledesma <aledesmadev@gmail.com>
2019-03-18 19:58:59 -0500

    Herencia prototipada

--- js/clase-1.js
+++ js/clase-1.js
@@ -1,4 +1,12 @@
-/* PROTOTIPOS EN JAVASCRIPT */
+/* HERENCIA PROTOTIPAL */
+function heredaDe(prototipoHijo, prototipoPadre) {
+    // eslint-disable-next-line no-empty-function
+    var fn = function() {};
+    fn.prototype = prototipoPadre.prototype;
+    prototipoHijo.prototype= new fn;
+    prototipoHijo.prototype.constructor = prototipoHijo;
+}
+
 function persona(nombre, apellido, estatura, ocupacion) {
     this.nombre = nombre;
     this.apellido = apellido;
@@ -20,8 +28,22 @@ persona.prototype.myAltura = function () {
         console.log(`mi estatura es ${this.estatura}, por tal motivo soy una persona alta`);
     }
 }
-const anibal = new persona('Anibal', 'Ledesma', 1.69);
-const angela = new persona('Angela', 'Ledesma', 1.55);
 
-anibal.saludar();
-angela.saludar();
+function desarrollador(nombre, apellido, estatura) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.estatura = estatura;
+}
+
+heredaDe(desarrollador, persona);
+
+desarrollador.prototype.saludar = function () {
+        // eslint-disable-next-line no-console
+    console.log(`Hola mi nombre es ${this.nombre} ${this.apellido} y soy un desarrollador`);
+}
+
+// const anibal = new desarrollador('Anibal', 'Ledesma', 1.69);
+// const angela = new persona('Angela', 'Ledesma', 1.55);
+
+// anibal.saludar();
+// angela.saludar();
