ecdffb07849aea3196ee15cef8f20ceb1d6ec04d
Anibal Ledesma <aledesmadev@gmail.com>
2019-03-18 20:36:51 -0500

    Clases y Herencia

--- js/clase-1.js
+++ js/clase-1.js
@@ -1,49 +1,46 @@
-/* HERENCIA PROTOTIPAL */
-function heredaDe(prototipoHijo, prototipoPadre) {
-    // eslint-disable-next-line no-empty-function
-    var fn = function() {};
-    fn.prototype = prototipoPadre.prototype;
-    prototipoHijo.prototype= new fn;
-    prototipoHijo.prototype.constructor = prototipoHijo;
-}
+/* CLASES Y HERENCIA */
+class persona {
+    constructor(nombre, apellido, estatura, ocupacion) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.estatura = estatura;
+        this.ocupacion = ocupacion;
+    }
 
-function persona(nombre, apellido, estatura, ocupacion) {
-    this.nombre = nombre;
-    this.apellido = apellido;
-    this.estatura = estatura;
-    this.ocupacion = ocupacion;
-}
-persona.prototype.saludar = function () {
-    // eslint-disable-next-line no-console
-    console.log(`Hola mi nombre es ${this.nombre} ${this.apellido}.`);
-}
+    saludar() {
+        // eslint-disable-next-line no-console
+        console.log(`Hola mi nombre es ${this.nombre} ${this.apellido}.`);
+    }
 
-persona.prototype.myAltura = function () {
-    var myEstatura = this.estatura * 100;
-    if (myEstatura <= 155) {
-        // eslint-disable-next-line no-console
-        console.log(`mi estatura es ${this.estatura}, por tal motivo soy una persona bajita`);
-    } else {
-        // eslint-disable-next-line no-console
-        console.log(`mi estatura es ${this.estatura}, por tal motivo soy una persona alta`);
+
+    myAltura () {
+        var myEstatura = this.estatura * 100;
+        if (myEstatura <= 155) {
+            // eslint-disable-next-line no-console
+            console.log(`mi estatura es ${this.estatura}, por tal motivo soy una persona bajita`);
+        } else {
+            // eslint-disable-next-line no-console
+            console.log(`mi estatura es ${this.estatura}, por tal motivo soy una persona alta`);
+        }
     }
 }
 
-function desarrollador(nombre, apellido, estatura) {
-    this.nombre = nombre;
-    this.apellido = apellido;
-    this.estatura = estatura;
-}
 
-heredaDe(desarrollador, persona);
+class desarrollador extends persona {
+    constructor(nombre, apellido, estatura) {
+        super(nombre, apellido, estatura)
+        // this.nombre = nombre;
+        // this.apellido = apellido;
+        // this.estatura = estatura;
+    }
 
-desarrollador.prototype.saludar = function () {
+    saludar () {
         // eslint-disable-next-line no-console
-    console.log(`Hola mi nombre es ${this.nombre} ${this.apellido} y soy un desarrollador`);
+        console.log(`Hola mi nombre es ${this.nombre} ${this.apellido} y soy un desarrollador`);
+    }
 }
 
-// const anibal = new desarrollador('Anibal', 'Ledesma', 1.69);
-// const angela = new persona('Angela', 'Ledesma', 1.55);
-
-// anibal.saludar();
-// angela.saludar();
+// eslint-disable-next-line no-unused-vars
+const anibal = new desarrollador('Anibal', 'Ledesma', 1.69);
+// eslint-disable-next-line no-unused-vars
+const angela = new persona('Angela', 'Ledesma', 1.55);
