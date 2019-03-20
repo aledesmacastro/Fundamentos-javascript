2a30288c52bb6f2654b17eb84d84cc3b0d9414be
Anibal Ledesma <aledesmadev@gmail.com>
2019-03-19 21:53:00 -0500

    manejando el orden y el asincronismo de javascript

--- js/clase-1.js
+++ js/clase-1.js
@@ -1,18 +1,31 @@
-/* HACIENDO MULTIPLES REQUEST*/
+/* MANEJANDO EL ORDEN Y EL ASINCRONISMO DE JAVASCRIPT*/
 const API_URL = 'https://swapi.co/api/';
 const PEOPLE_URL = 'people/:id';
 const opts = { crossDomain: true };
 // const lukeUrl = `${API_URL}${PEOPLE_URL.replace(':id', 4)}`;
 
-// eslint-disable-next-line no-console
-const onResponse = personaje => console.log(`Hola me llamo ${personaje.name}`);
-function obtenerPersonaje(id) {
+function obtenerPersonaje(id, callback) {
     const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`;
+    // eslint-disable-next-line no-console
+    // const onResponse = personaje => console.log(`Hola me llamo ${personaje.name}`);
     // eslint-disable-next-line no-undef
-    $.get(url, opts, onResponse);
+    $.get(url, opts, function (personaje) {
+         // eslint-disable-next-line no-console
+        console.log(`Hola me llamo ${personaje.name}`);
+
+        if(callback) {
+            callback();
+        }
+    });
 }
-obtenerPersonaje(1);
-obtenerPersonaje(2);
-obtenerPersonaje(3);
-obtenerPersonaje(4);
-obtenerPersonaje(5);
+obtenerPersonaje(1, function () {
+    obtenerPersonaje(2, function() {
+        obtenerPersonaje(3, function() {
+            obtenerPersonaje(4, function() {
+                obtenerPersonaje(5, function () {
+                    obtenerPersonaje(6)
+                })
+            })
+        })
+    })
+})
