7adc0fb994ff7d7d7f0e7ba7f48932577252b487
Anibal Ledesma <aledesmadev@gmail.com>
2019-03-20 11:18:45 -0500

    Promesas encadenadas

--- js/clase-1.js
+++ js/clase-1.js
@@ -1,4 +1,4 @@
-/* PROMESAS*/
+/* PROMESAS ENCADENADAS*/
 const API_URL = 'https://swapi.co/api/';
 const PEOPLE_URL = 'people/:id';
 const opts = { crossDomain: true };
@@ -20,7 +20,24 @@ function onError(id) {
     console.log(`Sucedio un error al obtener el personaje  ${id}`);
 }
 
-obtenerPersonaje(1).then((personaje) => {
+obtenerPersonaje(1).then(personaje => {
     // eslint-disable-next-line no-console
-    console.log(`Hola me llamo ${personaje.name}`);
-}).catch((onError))
+    console.log(`Hola me llamo ${personaje.name} y soy el personaje 1`);
+    return obtenerPersonaje(2)
+}).then(personaje2 => {
+    // eslint-disable-next-line no-console
+    console.log(`Hola me llamo ${personaje2.name} y soy el personaje 2`);
+    return obtenerPersonaje(3)
+}).then(personaje3 => {
+    // eslint-disable-next-line no-console
+    console.log(`Hola me llamo ${personaje3.name} y soy el personaje 3`);
+    return obtenerPersonaje(4)
+}).then(personaje4 => {
+    // eslint-disable-next-line no-console
+    console.log(`Hola me llamo ${personaje4.name} y soy el personaje 4`);
+    return obtenerPersonaje(5)
+}).then(personaje5 => {
+    // eslint-disable-next-line no-console
+    console.log(`Hola me llamo ${personaje5.name} y soy el personaje 5`);
+})
+.catch((onError))
